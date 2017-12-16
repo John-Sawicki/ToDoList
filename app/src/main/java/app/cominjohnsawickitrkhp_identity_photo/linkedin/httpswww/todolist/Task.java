31627ec95092fecb27b6e523b6f4d792333f13a5
john-sawicki <3Gxapnil>
2017-12-16 16:19:40 -0600

    added getters and setters to Task.java

--- app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/todolist/Task.java
+++ app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/todolist/Task.java
@@ -10,4 +10,35 @@ public class Task {
         this.color = color;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 }
