4b3a842cee859c4689c87a701d3650081496799a
john-sawicki <3Gxapnil>
2017-12-16 16:10:20 -0600

    Initial commit

---
+++ app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/todolist/Task.java
@@ -0,0 +1,12 @@
+package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.todolist;
+
+public class Task {
+    private String date, category, description;
+    private int color;
+    Task(String date, String category, String description, int color){
+        this.date = date;
+        this.category = category;
+        this.description = description;
+        this.color = color;
+    }
+}
